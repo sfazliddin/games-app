8fa62e2eae3b64d99591daba8f62e5fb3c697c7e
Samariddin <gotenks.ssj499@gmail.com>
2023-11-28 17:44:51 -0500

    added more styling and a settings page

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 
 const HomeScreen: React.FC = () => {
   return (
-    <div className="container mx-auto mt-10 text-center">
-      <h1 className="text-4xl font-bold mb-6">Welcome to the Game App</h1>
-      <div className="grid grid-cols-1 gap-4">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-6xl font-extrabold mb-8 text-center">
+        Welcome to the Game App
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <Link to="/1-player" className="btn">
-          Play 1 Player
+          1 Player Games
         </Link>
         <Link to="/2-player" className="btn">
-          Play 2 Player
+          2 Player Games
         </Link>
         <Link to="/settings" className="btn">
           Settings
--- src/components/SettingsScreen.tsx
+++ src/components/SettingsScreen.tsx
@@ -1,11 +1,61 @@
 // src/components/SettingsScreen.tsx
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 const SettingsScreen: React.FC = () => {
-  // Implement your settings logic here
+  const [darkMode, setDarkMode] = useState(false);
+  const [fontSize, setFontSize] = useState("medium");
+
+  useEffect(() => {
+    // Apply dark mode class to the body when darkMode changes
+    document.body.classList.toggle("dark", darkMode);
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
+  const changeFontSize = (size: string) => {
+    setFontSize(size);
+  };
+
   return (
-    <div>
-      <h1>Settings Screen</h1>
+    <div className="container mx-auto mt-10">
+      <h1 className="text-4xl font-bold mb-6">Settings</h1>
+
+      <div className="mb-4 relative">
+        <label className="mr-2">Dark Mode:</label>
+        <button
+          type="button"
+          onClick={toggleDarkMode}
+          className={`dark-toggler w-16 h-8 flex items-center justify-center rounded-full ${
+            darkMode ? "bg-gray-800" : "bg-gray-200"
+          }`}
+        >
+          {darkMode ? (
+            <MoonIcon className="w-4 h-4 text-gray-200" />
+          ) : (
+            <SunIcon className="w-4 h-4 text-gray-800" />
+          )}
+        </button>
+      </div>
+
+      <div className="mb-4">
+        <label className="mr-2">Font Size:</label>
+        <select
+          value={fontSize}
+          onChange={(e) => changeFontSize(e.target.value)}
+          className="px-2 py-1 border rounded"
+        >
+          <option value="small">Small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+        </select>
+      </div>
+
+      <div className="mb-4">
+        <p>Other settings go here...</p>
+      </div>
     </div>
   );
 };
