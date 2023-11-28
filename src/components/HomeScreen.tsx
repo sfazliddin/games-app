f86d83b8792c7e9c8ef54139686c5b675c0a6a2d
Samariddin <gotenks.ssj499@gmail.com>
2023-11-28 17:13:46 -0500

    added basic styling using tailwind

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -4,12 +4,22 @@ import { Link } from "react-router-dom";
 
 const HomeScreen: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the Game App</h1>
-      <Link to="/1-player">Play 1 Player</Link>
-      <Link to="/2-player">Play 2 Player</Link>
-      <Link to="/settings">Settings</Link>
-      <Link to="/feedback">Give Feedback</Link>
+    <div className="container mx-auto mt-10 text-center">
+      <h1 className="text-4xl font-bold mb-6">Welcome to the Game App</h1>
+      <div className="grid grid-cols-1 gap-4">
+        <Link to="/1-player" className="btn">
+          Play 1 Player
+        </Link>
+        <Link to="/2-player" className="btn">
+          Play 2 Player
+        </Link>
+        <Link to="/settings" className="btn">
+          Settings
+        </Link>
+        <Link to="/feedback" className="btn">
+          Give Feedback
+        </Link>
+      </div>
     </div>
   );
 };
